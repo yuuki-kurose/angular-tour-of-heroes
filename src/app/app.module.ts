4324539d299f5c6dd3dd9f1baad794bc75fce81a
yuuki kurose <pw1006@icloud.com>
2022-08-05 17:19:47 +0900

    ライフサイクルフックでHttpClientを利用しヒーローの情報を取得する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { AppComponent }         from './app.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { MessageComponent }     from './message/message.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
+import { AppComponent }                   from './app.component';
+import { HeroesComponent }                from './heroes/heroes.component';
+import { HeroDetailComponent }            from './hero-detail/hero-detail.component';
+import { MessageComponent }               from './message/message.component';
+import { DashboardComponent }             from './dashboard/dashboard.component';
 
-import { AppRoutingModule }     from './app-routing.module';
-import { NgModule }             from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
-import { FormsModule }          from '@angular/forms';
-import { HttpClientModule }     from '@angular/common/http';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { AppRoutingModule }               from './app-routing.module';
+import { NgModule }                       from '@angular/core';
+import { BrowserModule }                  from '@angular/platform-browser';
+import { FormsModule }                    from '@angular/forms';
+import { HttpClientModule }               from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }            from './in-memory-data.service';
 
 
 @NgModule({
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,18 @@
-import { Injectable }     from '@angular/core';
-import { Hero }           from './hero';
-import { HEROES }         from './mock-heroes';
-import { Observable, of } from 'rxjs';
-import { MessageService } from './message.service';
+import { Injectable }               from '@angular/core';
+import { Hero }                     from './hero';
+import { HEROES }                   from './mock-heroes';
+import { Observable, of }           from 'rxjs';
+import { MessageService }           from './message.service';
+import { HttpClient, HttpHeaders }  from '@angular/cummon/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
@@ -19,9 +22,7 @@ export class HeroService {
   
   // of関数を使用し、サーバーからのデータの取得→addコマンドにより送信
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)    
   }
 
   
