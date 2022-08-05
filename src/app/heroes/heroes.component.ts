7920ef9db102202edca5efade70d62a5699b6fda
yuuki kurose <pw1006@icloud.com>
2022-08-05 10:56:34 +0900

    インデント修正、メッセージサービス未追加の部分修正、ナビゲーションのルートの設定

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,35 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero }              from '../hero';
 import { HeroService }       from '../hero.service';
+import { MessageService }    from '../message.service';
+
+
 @Component({
   selector: 　　'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: 　['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // heroプロパティを追加する
-  hero: Hero = { id: 1, name: '黒瀬有紀' };
+  
+  // クリックイベントにより、メソッドを設定する
+  // selectedHeroは省略可能プロパティである
+  selectedHero?: Hero;
 
   // heroesコンポーネントプロパティを定義し、HEROES配列を表示
   heroes: Hero[] = [];
 
-  // クリックイベントにより、メソッドを設定する
-  // selectedHeroは省略可能プロパティである
-  selectedHero?: Hero;
+  // HeroesComponentが生成される際、privateなheroServiceプロパティを定義
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
   }
-  // HeroesComponentが生成される際、privateなheroServiceプロパティを定義
-  constructor(private heroService: HeroService) { }
 
   // サービスを介してヒーローデータを取得するためのメソッド
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
